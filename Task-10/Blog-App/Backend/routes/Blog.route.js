1160cb0672ce85a45b21828638764430a86e4f40
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-10-03 17:21:16 +0530

    add task 10

---
+++ Task-10/Blog-App/Backend/routes/Blog.route.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const { BlogModel } = require('../models/Blog.model');
+const BlogRouter = express.Router();
+
+// POST a new blog post
+BlogRouter.post('/', async (req, res) => {
+    try {
+
+        const newBlog = new BlogModel(req.body);
+        const savedBlog = await newBlog.save();
+        res.status(201).json({ message: "Blog Post succseccfull ", status: true, Blog: savedBlog });
+    } catch (error) {
+        console.error('Error creating a blog post:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
+// GET all blog posts
+BlogRouter.get('/', async (req, res) => {
+    try {
+        const blogs = await BlogModel.find();
+        res.status(200).json(blogs);
+    } catch (error) {
+        console.error('Error fetching blog posts:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
+// GET a specific blog post by ID
+BlogRouter.get('/:id', async (req, res) => {
+    try {
+        const blog = await BlogModel.findById(req.params.id);
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog post not found.' });
+        }
+        res.status(200).json(blog);
+    } catch (error) {
+        console.error('Error fetching a blog post:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
+// PUT (update) a specific blog post by ID
+BlogRouter.put('/:id', async (req, res) => {
+    try {
+        const updatedBlog = await BlogModel.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        if (!updatedBlog) {
+            return res.status(404).json({ message: 'Blog post not found.' });
+        }
+        res.status(200).json(updatedBlog);
+    } catch (error) {
+        console.error('Error updating a blog post:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
+// DELETE a specific blog post by ID
+BlogRouter.delete('/:id', async (req, res) => {
+    try {
+        const deletedBlog = await BlogModel.findByIdAndDelete(req.params.id);
+        if (!deletedBlog) {
+            return res.status(404).json({ message: 'Blog post not found.' });
+        }
+        res.status(204).send(); // No content in the response
+    } catch (error) {
+        console.error('Error deleting a blog post:', error);
+        res.status(500).json({ message: 'Internal server error.' });
+    }
+});
+
+module.exports = {
+    BlogRouter
+};
